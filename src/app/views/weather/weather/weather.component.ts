688d7af9e4bac4c2be679f0959fa2c1361677f59
Zeel Gajrawala <zeel.gajrawala.work@gmail.com>
2023-07-22 22:50:37 +0530

    Created html design and added required css style and tailwindcss config

--- src/app/views/weather/weather/weather.component.ts
+++ src/app/views/weather/weather/weather.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class WeatherComponent {
 
+  weatherType: string = '';
+  weekdays: string[] = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "10xl": "10rem",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+      width: {
+        600: "600px",
+        20: "20%",
+        8: "8%",
+      },
+      height: {
+        600: "600px",
+      },
+    },
+    fontFamily: {
+      yeseva: ["Yeseva One Regular"],
+      comfortaa: ["Comfortaa"],
+      barlow: ["Barlow Condensed"],
+    },
   },
   plugins: [],
 };
